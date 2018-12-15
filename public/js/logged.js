6e9d2ad4e454e80bc1b61d2235c2c0515b997ae4
eawwal <eakrumawwal@gmail.com>
2018-12-15 04:16:08 -0500

    log out func

--- public/js/logged.js
+++ public/js/logged.js
@@ -7,20 +7,18 @@ function getToken() {
   return token;
 }
 
-const checker = phrase => {                         //checks if a comment was even provided or not
-  if(!phrase) return false;
-  if (!phrase.replace(/\s/g, '').length) return false;
+const checker = phrase => {
+  //checks if a comment was even provided or not
+  if (!phrase) return false;
+  if (!phrase.replace(/\s/g, "").length) return false;
   return true;
-}
-
-
+};
 
 (function($) {
-
-  $(".USTop50SongFeed").attr('style','display: none');      //when page loads in the regular state, it hides all other categories data excep main feed
-  $(".GlobaTop50SongFeed").attr('style','display: none');   //when page loads in the regular state, it hides all other categories data excep main feed
-  $(".GlobaViral50SongFeed").attr('style','display: none');  //when page loads in the regular state, it hides all other categories data excep main feed
-  $(".USViral50SongFeed").attr('style','display: none');  //when page loads in the regular state, it hides all other categories data excep main feed
+  $(".USTop50SongFeed").attr("style", "display: none"); //when page loads in the regular state, it hides all other categories data excep main feed
+  $(".GlobaTop50SongFeed").attr("style", "display: none"); //when page loads in the regular state, it hides all other categories data excep main feed
+  $(".GlobaViral50SongFeed").attr("style", "display: none"); //when page loads in the regular state, it hides all other categories data excep main feed
+  $(".USViral50SongFeed").attr("style", "display: none"); //when page loads in the regular state, it hides all other categories data excep main feed
 
   $(".user-added-comment").click(function(event) {
     event.preventDefault();
@@ -29,7 +27,7 @@ const checker = phrase => {                         //checks if a comment was ev
       .text()
       .slice(5);
     let buttonParentID = classes[4].id; //finds the id of the parent class where button was clicked
-    console.log(buttonParentID)
+    console.log(buttonParentID);
     let findDatabaseId = $(`#${buttonParentID}`)
       .find("input#id_placeholder")
       .val(); //gets database reference id
@@ -39,93 +37,105 @@ const checker = phrase => {                         //checks if a comment was ev
       .serializeArray();
     let newlyAddedComment = formData[0].value; //gets new comment posated by user
 
-    let validComment = checker(newlyAddedComment)
+    let validComment = checker(newlyAddedComment);
 
     // console.log(findDatabaseId)
     // console.log(UserPostingNewComment)
     //  console.log(newlyAddedComment)
 
-    if(validComment){
+    if (validComment) {
+      $("#newlyMadeComment").css({
+        "border-color": "transparent",
+        "border-width": "3px",
+        "border-style": "solid"
+      });
 
-      $("#newlyMadeComment").css({"border-color": "transparent", 
-             "border-width":"3px", 
-             "border-style":"solid"});
-
-    $.ajax({
-      type: "Post",
-      url: "/songs/comments",
-      contentType: "application/json",
-      processData: false,
-      data: JSON.stringify({
-        dataBaseID: findDatabaseId,
-        UserPostingNewComment: UserPostingNewComment,
-        newlyAddedComment: newlyAddedComment
-      }),
-      dataType: "json",
-      success: function(body) {
-        //THE DATABASE NEEDS TO RETURN A JSON
-        location.href = "/login";
-      },
-      error: function() {
-        console.log("Error hitting the post route for comments");
-      }
-    });
-
+      $.ajax({
+        type: "Post",
+        url: "/songs/comments",
+        contentType: "application/json",
+        processData: false,
+        data: JSON.stringify({
+          dataBaseID: findDatabaseId,
+          UserPostingNewComment: UserPostingNewComment,
+          newlyAddedComment: newlyAddedComment
+        }),
+        dataType: "json",
+        success: function(body) {
+          //THE DATABASE NEEDS TO RETURN A JSON
+          location.href = "/login";
+        },
+        error: function() {
+          console.log("Error hitting the post route for comments");
+        }
+      });
     } else {
+      console.log("Valid comment wasnt provided");
+      $("#newlyMadeComment").css({
+        "border-color": "#FF0000",
+        "border-width": "3px",
+        "border-style": "solid"
+      });
 
-      console.log("Valid comment wasnt provided")
-      $("#newlyMadeComment").css({"border-color": "#FF0000", 
-             "border-width":"3px", 
-             "border-style":"solid"});
-
-      $("input#newlyMadeComment").attr("placeholder", "Comment Was not provided")
+      $("input#newlyMadeComment").attr(
+        "placeholder",
+        "Comment Was not provided"
+      );
     }
-
   });
 
-  $("#UsTop50Category").click(function(event) {           //upon click of category it hides all other content and only displays USTop50 data
-      $(".GlobaTop50SongFeed").attr('style','display: none');
-      $(".GlobaViral50SongFeed").attr('style','display: none');
-      $(".USViral50SongFeed").attr('style','display: none');
-      $(".RegularSongFeed").attr('style','display: none');
-      $(".USTop50SongFeed").removeAttr("style");
-     });
+  $("#UsTop50Category").click(function(event) {
+    //upon click of category it hides all other content and only displays USTop50 data
+    $(".GlobaTop50SongFeed").attr("style", "display: none");
+    $(".GlobaViral50SongFeed").attr("style", "display: none");
+    $(".USViral50SongFeed").attr("style", "display: none");
+    $(".RegularSongFeed").attr("style", "display: none");
+    $(".USTop50SongFeed").removeAttr("style");
+  });
 
-  $("#GlobalTop50Category").click(function(event) {  //upon click of category it hides all other content and only displays GlobalTop50 data
-      $(".GlobaTop50SongFeed").removeAttr("style");
-      $(".GlobaViral50SongFeed").attr('style','display: none');
-      $(".USViral50SongFeed").attr('style','display: none');
-      $(".RegularSongFeed").attr('style','display: none');
-      $(".USTop50SongFeed").attr('style','display: none');
-     });
+  $("#GlobalTop50Category").click(function(event) {
+    //upon click of category it hides all other content and only displays GlobalTop50 data
+    $(".GlobaTop50SongFeed").removeAttr("style");
+    $(".GlobaViral50SongFeed").attr("style", "display: none");
+    $(".USViral50SongFeed").attr("style", "display: none");
+    $(".RegularSongFeed").attr("style", "display: none");
+    $(".USTop50SongFeed").attr("style", "display: none");
+  });
 
-  $("#GlobalViral50Category").click(function(event) {        //upon click of category it hides all other content and only displays GlobalViral50 data
-      $(".GlobaTop50SongFeed").attr('style','display: none');
-      $(".GlobaViral50SongFeed").removeAttr("style");
-      $(".USViral50SongFeed").attr('style','display: none');
-      $(".RegularSongFeed").attr('style','display: none');
-      $(".USTop50SongFeed").attr('style','display: none');
-     });
+  $("#GlobalViral50Category").click(function(event) {
+    //upon click of category it hides all other content and only displays GlobalViral50 data
+    $(".GlobaTop50SongFeed").attr("style", "display: none");
+    $(".GlobaViral50SongFeed").removeAttr("style");
+    $(".USViral50SongFeed").attr("style", "display: none");
+    $(".RegularSongFeed").attr("style", "display: none");
+    $(".USTop50SongFeed").attr("style", "display: none");
+  });
 
-  $("#UsViral50Category").click(function(event) {                   //upon click of category it hides all other content and only displays USViral50 data
-      $(".GlobaTop50SongFeed").attr('style','display: none');
-      $(".GlobaViral50SongFeed").attr('style','display: none');
-      $(".USViral50SongFeed").removeAttr("style");
-      $(".RegularSongFeed").attr('style','display: none');
-      $(".USTop50SongFeed").attr('style','display: none');
-     });
+  $("#UsViral50Category").click(function(event) {
+    //upon click of category it hides all other content and only displays USViral50 data
+    $(".GlobaTop50SongFeed").attr("style", "display: none");
+    $(".GlobaViral50SongFeed").attr("style", "display: none");
+    $(".USViral50SongFeed").removeAttr("style");
+    $(".RegularSongFeed").attr("style", "display: none");
+    $(".USTop50SongFeed").attr("style", "display: none");
+  });
 
-  $("#RegularCategory").click(function(event) {                      //upon click of category it hides all other content and only displays Whole song feed data
-      $(".GlobaTop50SongFeed").attr('style','display: none');
-      $(".GlobaViral50SongFeed").attr('style','display: none');
-      $(".USViral50SongFeed").attr('style','display: none');
-      $(".RegularSongFeed").removeAttr("style");
-      $(".USTop50SongFeed").attr('style','display: none');
-     });
-
-
+  $("#RegularCategory").click(function(event) {
+    //upon click of category it hides all other content and only displays Whole song feed data
+    $(".GlobaTop50SongFeed").attr("style", "display: none");
+    $(".GlobaViral50SongFeed").attr("style", "display: none");
+    $(".USViral50SongFeed").attr("style", "display: none");
+    $(".RegularSongFeed").removeAttr("style");
+    $(".USTop50SongFeed").attr("style", "display: none");
+  });
 
   // $(".user-added-comment").click(function() {
   //   console.log("yes this works")
   // });
 })(jQuery);
+
+function logOut() {
+  document.cookie = "";
+  location.href = "/";
+  console.log("cookie?", document.cookie);
+}
--- public/js/playlist.js
+++ public/js/playlist.js
@@ -1,99 +1,98 @@
+(function($) {
+  for (let i = 0; i <= 4; i++) {
+    $(`#${i} .card-header`).html(i + 1);
+  }
 
+  for (let i = 10; i <= 14; i++) {
+    let number = i - 4;
+    $(`#${i} .card-header`).html(number);
+  }
 
-(function ($){
+  for (let i = 20; i <= 24; i++) {
+    let number = i - 9;
+    $(`#${i} .card-header`).html(number);
+  }
 
-    for(let i=0; i<=4; i++){
-        $(`#${i} .card-header`).html(i + 1)
+  for (let i = 30; i <= 34; i++) {
+    let number = i - 14;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 40; i <= 44; i++) {
+    let number = i - 19;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 50; i <= 54; i++) {
+    let number = i - 24;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 60; i <= 64; i++) {
+    let number = i - 29;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 70; i <= 74; i++) {
+    let number = i - 34;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 80; i <= 84; i++) {
+    let number = i - 39;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  for (let i = 90; i <= 94; i++) {
+    let number = i - 44;
+    $(`#${i} .card-header`).html(number);
+  }
+
+  $(".button-song").click(function() {
+    let classes = $(this).parents();
+    let buttonParentID = classes[2].id; //finds the id of the parent class where button was clicked
+    let findAttribute = $(`#${buttonParentID}`).find("#Name"); //finds the attribute under this id
+    let getNameOfSong = findAttribute[0].textContent; //gets text content
+    $("#SongName").text("Sharing Song:  " + getNameOfSong); //adds the name of the song to the popup
+
+    let findSongid = $(`#${buttonParentID}`)
+      .find("input#songid")
+      .val(); //finds song id for form to then send back for us to use\
+    $("#passBackId").attr("value", findSongid); //sends the id to the input field in the form
+  });
+
+  $(".sharing-specific-song").click(function(event) {
+    event.preventDefault();
+    let formData = $("form#form-submit-comment").serializeArray();
+    let SharingObject = {};
+    for (let i = 0; i < formData.length; i++) {
+      let variableName = formData[i].name;
+      let variableData = formData[i].value;
+      SharingObject[`${variableName}`] = variableData;
     }
 
-    for(let i=10; i<=14; i++){
-        let number = i - 4;
-        $(`#${i} .card-header`).html(number)
-    }
+    //   console.log(SharingObject)
 
-    for(let i=20; i<=24; i++){
-        let number = i - 9;
-        $(`#${i} .card-header`).html(number)
-    }
+    $.ajax({
+      //ajax call to the songs route
+      type: "Post",
+      url: "/songs",
+      contentType: "application/json",
+      processData: false,
+      data: JSON.stringify(SharingObject),
+      success: function(data) {
+        console.log("shared: ", data);
+      },
+      error: function() {
+        console.log("Error hitting the post route for comments");
+      }
+    });
 
-    for(let i=30; i<=34; i++){
-        let number = i - 14;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=40; i<=44; i++){
-        let number = i - 19;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=50; i<=54; i++){
-        let number = i - 24;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=60; i<=64; i++){
-        let number = i - 29;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=70; i<=74; i++){
-        let number = i - 34;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=80; i<=84; i++){
-        let number = i - 39;
-        $(`#${i} .card-header`).html(number)
-    }
-
-    for(let i=90; i<=94; i++){
-        let number = i - 44;
-        $(`#${i} .card-header`).html(number)
-    }
-
-
-    $(".button-song").click(function() {
-        let classes = $(this).parents()
-        let buttonParentID = classes[2].id //finds the id of the parent class where button was clicked
-        let findAttribute =  $(`#${buttonParentID}`).find("#Name")  //finds the attribute under this id
-        let getNameOfSong = findAttribute[0].textContent  //gets text content
-        $("#SongName").text("Sharing Song:  " + getNameOfSong) //adds the name of the song to the popup
-
-        let findSongid = $(`#${buttonParentID}`).find("input#songid").val()  //finds song id for form to then send back for us to use\
-        $("#passBackId").attr("value", findSongid)   //sends the id to the input field in the form
-
-    }); 
-    
-    $(".sharing-specific-song").click(function(event) {
-        event.preventDefault();
-        let formData = $("form#form-submit-comment").serializeArray()
-        let SharingObject = {}
-        for(let i=0; i<formData.length; i++){
-            let variableName = formData[i].name
-            let variableData = formData[i].value
-           SharingObject[`${variableName}`] = variableData;
-        }
-
-     //   console.log(SharingObject)
-
- 
-         $.ajax({                                                   //ajax call to the songs route
-             type: "Post", 
-             url: "/songs",
-             contentType: 'application/json',
-             processData: false,
-             data: JSON.stringify(SharingObject),
-             success: function(data) {
-                 console.log("shared: ", data)
-             },
-             error: function() {
-              console.log("Error hitting the post route for comments");
-             }
-          });
-
-         $("#ModalCenter").modal("hide") 
-
-    })
-    
-
+    $("#ModalCenter").modal("hide");
+  });
 })(jQuery);
+function logOut() {
+  document.cookie = "";
+  location.href = "/";
+  console.log("cookie?", document.cookie);
+}
